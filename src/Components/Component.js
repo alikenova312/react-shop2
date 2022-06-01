8c73b4896ecd1d13127101cf7fd66565e8d67a9f
alikenova312 <alikenova312@icloud.com>
2022-06-01 17:23:49 +0600

    component

---
+++ src/Components/Component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div>
+            <button className="btn">add</button>
+        </div>
+    );
+};
+
+export default Component;
